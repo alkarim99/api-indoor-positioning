d6679978eb81319d0c4c4b1597007d50a27cea7b
Abdullah Al-Karim Amrullah <abdullahalkarimamrullah01@gmail.com>
2024-06-05 20:52:40 +0800

    fix: deployment

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const authRoutes = require("./routes/auth.routes")
 const port = 3100
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: false }))
 
 // parse application/json
 app.use(bodyParser.json())
